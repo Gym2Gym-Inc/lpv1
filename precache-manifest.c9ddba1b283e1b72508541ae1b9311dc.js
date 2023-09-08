e83b17612f3e31fb7fb109c7eba95657df3d180c
ExeLxrd <tech.mccune@gmail.com>
2023-09-08 17:02:46 -0600

    Updates

---
+++ precache-manifest.c9ddba1b283e1b72508541ae1b9311dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a23d77edba515f59827ad42e3a53f35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e819f62ad723c64dd329",
+    "url": "/static/css/main.ef91e47c.chunk.css"
+  },
+  {
+    "revision": "4a60b28e685d11f9d65d",
+    "url": "/static/js/2.9fcd8957.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.9fcd8957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e819f62ad723c64dd329",
+    "url": "/static/js/main.560c4398.chunk.js"
+  },
+  {
+    "revision": "fb13463df14981ecad77",
+    "url": "/static/js/runtime-main.5cb864d5.js"
+  },
+  {
+    "revision": "b732bf72292bbccac0d93807811b4864",
+    "url": "/static/media/download__14_-removebg-preview.b732bf72.svg"
+  },
+  {
+    "revision": "06d64c2278ccf9a440645272dedaba7b",
+    "url": "/static/media/mail.06d64c22.svg"
+  },
+  {
+    "revision": "cf89919fb4529896970ebf172ff0672a",
+    "url": "/static/media/newsletter.cf89919f.svg"
+  },
+  {
+    "revision": "71f30eae279c44ee3362b43ef27f4c04",
+    "url": "/static/media/privacy.71f30eae.md"
+  },
+  {
+    "revision": "bab725b9add023d156a4e565f4b2ddbd",
+    "url": "/static/media/twitter.bab725b9.svg"
+  }
+]);
